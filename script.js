012a37fb657c08adaa5b2d5e99ee16b0924f2650
Pavel Darii <paveldarii@yahoo.com>
2020-11-10 12:00:09 -0500

    85% of the work is done, functionality is pressent

---
+++ script.js
@@ -0,0 +1,41 @@
+var plannerList = [
+  "#eightam",
+  "#nineam",
+  "#tenam",
+  "#elevenam",
+  "#twelvepm",
+  "#onepm",
+  "#twopm",
+  "#threepm",
+  "#fourpm",
+  "#fivepm",
+];
+
+var buttonEl = $("saveBtn");
+//bellow is the code to make the right color for the planner hours
+for (let i = 0; i < plannerList.length; i++) {
+  if (
+    moment().format("h") + moment().format("a") !==
+    $(plannerList[i] + " p").attr("value")
+  ) {
+    $(plannerList[i] + " textarea").css("background-color", "#DCDCDC");
+  } else if (
+    moment().format("h") + moment().format("a") ===
+    $(plannerList[i] + " p").attr("value")
+  ) {
+    $(plannerList[i] + " textarea").css("background-color", "#ff6262");
+    break;
+  }
+}
+
+// the code bellow set the text content from localStorage to the textarea
+for (let i = 0; i < plannerList.length; i++) {
+  $(plannerList[i] + " textarea").text(localStorage.getItem(plannerList[i]));
+}
+
+$(".saveBtn").on("click", function () {
+  var textareaEl = $("#" + $(this).parent().parent().attr("id") + " textarea");
+  var keyToStore = "#" + $(this).parent().parent().attr("id");
+  var textToStore = textareaEl.val();
+  localStorage.setItem(keyToStore, textToStore);
+});
